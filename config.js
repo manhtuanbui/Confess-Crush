c5d2857a7295147a9fcbe7f566e69bba61648e3b
Manh Tuan <106939616+manhtuanbui@users.noreply.github.com>
2023-02-10 09:57:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Hi My Love",
     introTitle: 'Hi My Love',
-    introDesc: `Yêu một người không khó
-    Vì chẳng cần lý do
-    Tất cả tình cảm của anh
-    Chỉ dành cho em đó`,
+    introDesc: `Anh rất hạnh phúc vì được gặp em, em là điều tuyệt vời nhất trong cuộc sống của anh`,
     btnIntro: '^^Love You^^',
     title: 'Anh đã yêu em ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Anh đã yêu em ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Yêu anh',
     btnNo: 'Không nha ,
-    question: 'Em có yêu anh hông ,
+    question: 'Anh sẽ nắm tay em đến cuối con đường ,
     btnReply: 'Em yêu anh ',
     reply: 'Anh yêu em nhiều lắm đó ,
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối anh qua đón em nha.',
+    messDesc: 'Suốt đời chỉ yêu mình em.',
     btnAccept: 'Okiiii lun ',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
